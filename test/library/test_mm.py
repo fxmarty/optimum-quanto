4df88693ac1c6a1702c704d9f27d3086aaadaf3e
David Corvoysier <david@huggingface.co>
2024-07-12 08:24:05 +0000

    review(marlin): add unit test

--- test/library/test_mm.py
+++ test/library/test_mm.py
@@ -91,3 +91,70 @@ def test_gemm_fp16_int4(batch_size, tokens, in_features, out_features):
     pt_outputs = torch.matmul(inputs, other_t)
     # Verify the results are similar
     assert_similar(lib_outputs, pt_outputs, rtol=5e-3)
+
+
+def pack_fp8_as_int32(fp8_tensor: torch.Tensor) -> torch.Tensor:
+    """
+    Repack FP8 weights to gptq format (packed int32 elements).
+    """
+    assert fp8_tensor.dtype == torch.float8_e4m3fn
+
+    if fp8_tensor.shape[0] % 4 != 0:
+        raise ValueError(f"Leading tensor dimension is not divisable by 4: {fp8_tensor.shape[0]}")
+
+    # Reshape to prepare for packing
+    reshaped = fp8_tensor.reshape(-1, 4, *fp8_tensor.shape[1:])
+
+    # Convert fp8 to uint8 (byte) representation
+    byte_tensor = reshaped.view(torch.uint8)
+
+    # Pack 4 uint8 values into one int32
+    packed = torch.zeros(
+        fp8_tensor.shape[0] // 4,
+        fp8_tensor.shape[1],
+        dtype=torch.int32,
+        device=fp8_tensor.device,
+    )
+
+    for i in range(4):
+        packed.bitwise_or_(byte_tensor[:, i].to(torch.int32) << i * 8)
+
+    return packed
+
+
+@pytest.mark.skipif(
+    not torch.cuda.is_available() or torch.cuda.get_device_capability()[0] < 8,
+    reason="CUDA device >= sm80 not available",
+)
+@pytest.mark.parametrize("tokens", [1, 10, 128])
+@pytest.mark.parametrize("in_features, out_features", [(256, 256), (512, 256)])
+@pytest.mark.parametrize("dtype", [torch.bfloat16, torch.float16], ids=["bf16", "fp16"])
+def test_fp8_marlin(tokens, in_features, out_features, dtype):
+    device = torch.device("cuda")
+    input_shape = (tokens, in_features)
+    inputs = torch.rand(input_shape, dtype=dtype, device=device)
+    other_shape = (in_features, out_features)
+    other_data = torch.rand(other_shape, dtype=dtype, device=device).to(torch.float8_e4m3fn)
+    other_data_int32 = pack_fp8_as_int32(other_data)
+    perm = torch.empty(0, dtype=torch.int, device=device)
+    other_data_repack = torch.ops.quanto.gptq_marlin_repack(
+        b_q_weight=other_data_int32, perm=perm, size_k=in_features, size_n=out_features, num_bits=8
+    )
+    other_scale_shape = (1, out_features)
+    other_scale = torch.rand(other_scale_shape, dtype=dtype, device=device)
+    workspace = torch.zeros(out_features // 64 * 16, dtype=torch.int, device=device)
+    lib_outputs = torch.ops.quanto.fp8_marlin(
+        a=inputs,
+        b_q_weight=other_data_repack,
+        b_scales=other_scale,
+        workspace=workspace,
+        num_bits=8,
+        size_m=tokens,
+        size_n=out_features,
+        size_k=in_features,
+    )
+    # Evaluate the matrix multiplication using pytorch mm
+    other = other_data.to(dtype) * other_scale
+    pt_outputs = torch.matmul(inputs.to(dtype), other)
+    # Verify the results are similar
+    assert_similar(lib_outputs, pt_outputs)
